d5ca923bc52dfc84fb6e8c693d6b7be8dca4b766
makamat <maka.mat@proton.me>
2024-06-12 23:44:12 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,83 @@
-function defaultTask(cb) {
-    cb();
-}
+import gulp from "gulp";
+import { deleteAsync } from "del";
+import autoprefixer from "gulp-autoprefixer";
+import imagemin, { optipng } from "gulp-imagemin";
+import cssnano from "gulp-cssnano";
+import concat from "gulp-concat";
+import htmlmin from "gulp-htmlmin";
+import rename from "gulp-rename";
+import sourcemaps from "gulp-sourcemaps";
+import gulpreplace from "gulp-replace";
 
-exports.default = defaultTask;
+const paths = {
+    html: {
+        src: "src/**/*.html",
+        dest: "dist/",
+    },
+    css: {
+        src: "src/css/**/*.css",
+        dest: "dist/css/",
+    },
+    images: {
+        src: "src/images/**/*.png",
+        dest: "dist/images/",
+    },
+    fonts: {
+        src: "src/fonts/**/*.woff2",
+        dest: "dist/fonts",
+    },
+};
+
+const minifyHTML = async () => {
+    return gulp
+        .src(paths.html.src)
+        .pipe(gulpreplace("styles.css", "styles.min.css"))
+        .pipe(
+            htmlmin({
+                collapseWhitespace: true,
+                removeComments: true,
+                minifyJS: true,
+                minifyCSS: true,
+            })
+        )
+        .pipe(gulp.dest(paths.html.dest));
+};
+
+const styleCSS = async () => {
+    return gulp
+        .src(paths.css.src)
+        .pipe(sourcemaps.init())
+        .pipe(gulpreplace('@import url("sr-only.css");\n', ""))
+        .pipe(autoprefixer())
+        .pipe(concat("styles.css"))
+        .pipe(gulp.dest(paths.css.dest))
+        .pipe(cssnano())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest(paths.css.dest));
+};
+
+const optimizeImages = async () => {
+    return gulp
+        .src(paths.images.src)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.images.dest));
+};
+
+const cleanDist = async () => {
+    return deleteAsync(["dist"]);
+};
+
+const moveFonts = async () => {
+    return gulp.src(paths.fonts.src).pipe(gulp.dest(paths.fonts.dest));
+};
+
+gulp.task("clean", cleanDist);
+gulp.task("styleCSS", styleCSS);
+gulp.task(
+    "default",
+    gulp.series(
+        cleanDist,
+        gulp.parallel(minifyHTML, styleCSS, moveFonts, optimizeImages)
+    )
+);
